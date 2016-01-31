977abb5437542581016ecf883cffc259646e6883
Milan Milanov <milan.milanov@westernacher.com>
2016-01-31 14:51:11 +0200

    Try to fix scrolling issues

--- scripts/custom.js
+++ scripts/custom.js
@@ -15,11 +15,11 @@ $(window).load(function() {
 //------------------------------------------------------------------------
 //						WOW ANIMATION SETTINGS
 //------------------------------------------------------------------------ 	
-	var wow = new WOW({
-    	offset:100,        // distance to the element when triggering the animation (default is 0)
-    	mobile:false       // trigger animations on mobile devices (default is true)
-  	});
-	wow.init();
+	// var wow = new WOW({
+ //    	offset:100,        // distance to the element when triggering the animation (default is 0)
+ //    	mobile:false       // trigger animations on mobile devices (default is true)
+ //  	});
+	// wow.init();
 	
 
 	
@@ -73,16 +73,16 @@ $(window).load(function() {
 //------------------------------------------------------------------------
 //						NAVBAR SLIDE SCRIPT
 //------------------------------------------------------------------------ 		
-	$(window).scroll(function () {
-        if ($(window).scrollTop() > $("nav").height()) {
-            $("nav.navbar-slide").addClass("show-menu");
-        } else {
-            $("nav.navbar-slide").removeClass("show-menu");
-			$(".navbar-slide .navMenuCollapse").collapse({toggle: false});
-			$(".navbar-slide .navMenuCollapse").collapse("hide");
-			$(".navbar-slide .navbar-toggle").addClass("collapsed");
-        }
-    });
+	// $(window).scroll(function () {
+ //        if ($(window).scrollTop() > $("nav").height()) {
+ //            $("nav.navbar-slide").addClass("show-menu");
+ //        } else {
+ //            $("nav.navbar-slide").removeClass("show-menu");
+	// 		$(".navbar-slide .navMenuCollapse").collapse({toggle: false});
+	// 		$(".navbar-slide .navMenuCollapse").collapse("hide");
+	// 		$(".navbar-slide .navbar-toggle").addClass("collapsed");
+ //        }
+ //    });
 	
 //------------------------------------------------------------------------
 //						NAVBAR HIDE ON CLICK (COLLAPSED) SCRIPT
